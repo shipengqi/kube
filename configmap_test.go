622f834e8f61d89f2181a5c790e9dd4732c9dd97
shipengqi <pooky.shipengqi@gmail.com>
2022-01-05 14:36:31 +0800

    test: format import

--- configmap_test.go
+++ configmap_test.go
@@ -1,14 +1,14 @@
 package kube
 
 import (
+	"context"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"context"
-	"fmt"
 )
 
 func TestClientConfigmap(t *testing.T) {
