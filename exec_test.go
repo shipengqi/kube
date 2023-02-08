e1d7921b3a3abc3c4c60bccf24b19d274f2abe4e
shipengqi <pooky.shipengqi@gmail.com>
2023-02-08 14:49:01 +0800

    update exec tests

--- exec_test.go
+++ exec_test.go
@@ -8,18 +8,19 @@ import (
 )
 
 const (
-	_defaultTestPodName       = "nginx-for-exec-test"
-	_defaultTestContainerName = "nginx"
-	_defaultTestNamespace     = "kube-system"
+	_defaultTestNamespace = "kube-system"
 )
 
 func TestClientExec(t *testing.T) {
 	got, err := mockcli.GetPods(context.TODO(), _defaultTestNamespace)
 	require.NoError(t, err)
 	require.NotEqual(t, len(got.Items), 0)
-	t.Log(got.Items)
 
-	// stdout, _, err := mockcli.Exec(_defaultTestPodName, _defaultTestContainerName, _defaultTestNamespace, "ls /")
-	// require.NoError(t, err)
-	// t.Log(stdout)
+	firstPod := got.Items[0]
+	firstPodName := firstPod.Name
+	firstContainerName := firstPod.Spec.Containers[0].Name
+
+	stdout, _, err := mockcli.Exec(firstPodName, firstContainerName, _defaultTestNamespace, "ls /")
+	require.NoError(t, err)
+	t.Log(stdout)
 }
