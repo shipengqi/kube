58ff48e98ce46ffe579bb111d492baaa51e3592e
PengQi Shi <pooky.shipengqi@gmail.com>
2023-02-08 16:18:01 +0800

    chore: Add E2E test for Exec() (#9)
    * exec test
    
    * Add pod tests
    
    * update exec tests
    
    * update exec tests
    
    * update exec tests
    
    * update exec tests
    
    * update exec tests
    
    * update exec tests
    
    * update exec tests
    
    * update exec tests
    
    * update exec tests
    
    * update exec tests
    
    * update exec tests
    
    * update exec tests
    
    * use cache

--- exec_test.go
+++ exec_test.go
@@ -1,16 +1,39 @@
 package kube
 
 import (
+	"context"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
+const (
+	_defaultTestNamespace = "kube-system"
+	_defaultTestPod       = "coredns"
+)
+
 func TestClientExec(t *testing.T) {
-	if len(podname) == 0 || len(containername) == 0 || len(namespace) == 0 {
-		return
-	}
-	stdout, _, err := mockcli.Exec(podname, containername, namespace, "ls /")
+	list, err := mockcli.GetPods(context.TODO(), _defaultTestNamespace)
 	require.NoError(t, err)
+	require.NotEqual(t, len(list.Items), 0)
+
+	var (
+		podName       string
+		containerName string
+	)
+	// exec coredns
+	for _, v := range list.Items {
+		if strings.Contains(strings.ToLower(v.Name), _defaultTestPod) {
+			podName = v.Name
+			containerName = v.Spec.Containers[0].Name
+			break
+		}
+	}
+
+	// Todo, create a pod, and test Exec(), currently just skip validating the error
+	t.Log("exec:", podName, containerName)
+	stdout, _, err := mockcli.Exec(podName, containerName, _defaultTestNamespace, "/bin/sh", "-c", "/bin/ls /")
+	require.Error(t, err)
 	t.Log(stdout)
 }
