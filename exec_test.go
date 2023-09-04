66c69b70bfd6c75765580d102dd83f4bccb93f4c
shipengqi <pooky.shipengqi@gmail.com>
2023-09-04 17:53:31 +0800

    docker pull nginx image

--- exec_test.go
+++ exec_test.go
@@ -1,8 +1,6 @@
 package kube
 
 import (
-	"context"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -15,35 +13,36 @@ const (
 
 func TestClientExec(t *testing.T) {
 
-	var containerName string
+	containerName := "nginx"
+	podNamespace := "default"
+	podName := "nginx"
 
-	podNamespace := _defaultTestNamespace
-	podName := _defaultTestPod
+	// if namespace != "" {
+	// 	podNamespace = namespace
+	// }
+	//
+	// if podname != "" && containername != "" {
+	// 	podName = podname
+	// 	containerName = containername
+	// } else {
+	// 	list, err := mockcli.GetPods(context.TODO(), podNamespace)
+	// 	require.NoError(t, err)
+	// 	require.NotEqual(t, len(list.Items), 0)
+	//
+	// 	// exec coredns
+	// 	for _, v := range list.Items {
+	// 		if strings.Contains(strings.ToLower(v.Name), podName) {
+	// 			podName = v.Name
+	// 			containerName = v.Spec.Containers[0].Name
+	// 			break
+	// 		}
+	// 	}
+	// }
 
-	if namespace != "" {
-		podNamespace = namespace
-	}
-
-	if podname != "" && containername != "" {
-		podName = podname
-		containerName = containername
-	} else {
-		list, err := mockcli.GetPods(context.TODO(), podNamespace)
-		require.NoError(t, err)
-		require.NotEqual(t, len(list.Items), 0)
-
-		// exec coredns
-		for _, v := range list.Items {
-			if strings.Contains(strings.ToLower(v.Name), podName) {
-				podName = v.Name
-				containerName = v.Spec.Containers[0].Name
-				break
-			}
-		}
-	}
+	err := mockcli.Apply("testdata/pod-apply.yaml")
 
 	// Todo, create a pod, and test Exec(), currently just skip validating the error
-	t.Log("exec:", podName, containerName)
+	t.Log("exec:", "nginx", "nginx")
 	stdout, _, err := mockcli.Exec(podName, containerName, podNamespace, "/bin/sh", "-c", "/bin/ls /")
 	require.Error(t, err)
 	t.Log(stdout)
