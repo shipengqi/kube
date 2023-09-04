e0d964a93f9e3cbd9f6eea0396ca0597f53e76a3
shipengqi <pooky.shipengqi@gmail.com>
2023-09-04 17:58:29 +0800

    docker pull nginx image

--- exec_test.go
+++ exec_test.go
@@ -44,6 +44,7 @@ func TestClientExec(t *testing.T) {
 	// Todo, create a pod, and test Exec(), currently just skip validating the error
 	t.Log("exec:", "nginx", "nginx")
 	stdout, _, err := mockcli.Exec(podName, containerName, podNamespace, "/bin/sh", "-c", "/bin/ls /")
+	t.Log(err.Error())
 	require.Error(t, err)
 	t.Log(stdout)
 }
