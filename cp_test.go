01110e6e8d707ad249d3b0af7a3760015df0386c
shipengqi <pooky.shipengqi@gmail.com>
2023-09-08 13:55:05 +0800

    upload tests

--- cp_test.go
+++ cp_test.go
@@ -2,6 +2,8 @@ package kube
 
 import (
 	"context"
+	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -26,31 +28,27 @@ func TestCopy(t *testing.T) {
 		err := mockcli.Upload(context.TODO(), podName, containerName, podNamespace, "testdata/upload.txt", "/testdata")
 		require.NoError(t, err)
 
-		out, _, err := mockcli.Exec(podName, containerName, podNamespace, "ls", "-l", "/testdata")
+		out, _, err := mockcli.Exec(podName, containerName, podNamespace, "ls", "/testdata")
 		require.NoError(t, err)
-		t.Log(out)
+		assert.Contains(t, out, "upload.txt")
+
 	})
 
-	t.Run("upload file as file", func(t *testing.T) {
-		err := mockcli.Upload(context.TODO(), podName, containerName, podNamespace, "testdata/upload.txt", "/testdata/uploadtofile.txt")
-		require.NoError(t, err)
-
-		out, _, err := mockcli.Exec(podName, containerName, podNamespace, "ls", "-l", "/testdata/testdata")
-		require.NoError(t, err)
-		t.Log(out)
+	t.Run("should download err", func(t *testing.T) {
+		err := mockcli.Download(context.TODO(), podName, containerName, podNamespace, "testdata/noexists.txt", "testdata")
+		t.Log(err.Error())
 	})
 
-	// t.Run("download file", func(t *testing.T) {
-	// 	err := mockcli.Download(context.TODO(), podName, containerName, podNamespace, "testdata/upload.txt", "testdata")
-	// 	require.NoError(t, err)
-	// 	files, err := os.ReadDir("testdata")
-	// 	require.NoError(t, err)
-	// 	for _, v := range files {
-	// 		t.Log(v.Name(), v.IsDir())
-	// 	}
-	// })
-	// t.Run("should download err", func(t *testing.T) {
-	// 	err := mockcli.Download(context.TODO(), podName, containerName, podNamespace, "testdata/noexists.txt", "testdata")
-	// 	t.Log(err.Error())
-	// })
+	t.Run("download file", func(t *testing.T) {
+		err := mockcli.Download(context.TODO(), podName, containerName, podNamespace, "testdata/upload.txt", "testdata")
+		require.NoError(t, err)
+		files, err := os.ReadDir("testdata")
+		require.NoError(t, err)
+		for _, v := range files {
+			t.Log(v.Name(), v.IsDir())
+		}
+		_, _, err = mockcli.Exec(podName, containerName, podNamespace, "rm", "-rf", "/testdata")
+		require.NoError(t, err)
+	})
+
 }
